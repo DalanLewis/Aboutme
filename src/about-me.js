f6f72bc8c733b67d471018d3800597bd23bb5365
Dalan Lewis <dalanforpresident@gmail.com>
2018-12-06 18:07:21 -0700

    added aboutme and work experience page

---
+++ src/about-me.js
@@ -0,0 +1,59 @@
+import React, { Component, Fragment } from 'react';
+
+
+class aboutme extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                <h1>
+                    Dalan Lewis
+        </h1>
+            </header>
+            <article>
+                <img src="img/Marvin.png" alt="MarVln" />
+                <h2>
+                    Bio
+                </h2>
+                <p>
+                    Born and raised in Utah, I developed a passion for creativity from an early age. From writing stories, composing music, to my new passion Coding. I seek to develop myself into a invaluable asset to any potential employer or business partner. I seek to amaze with anything I put my hands on.
+                    </p>
+            </article>
+            <article>
+                <h2>
+                    Why Code?
+                </h2>
+                <p>
+                    It's simple. I believe the knowledge to code, coupled with my creativity will give me the tools I need to express myself in a marketable, and unforgetable way.
+                    </p>
+            </article>
+            <article>
+                <h2>
+                    Additional Info:
+            </h2>
+                <p>
+                    I have asked my closest friends and family to describe me in one word. I always seem to get the same answer: Passionate. I honestly cannot argue with that. Any free time I have is spent playing on my guitar, or with my small family. (I often play video games as well) One day I hope to provide them with all their hopes and dreams on a silver platter.
+                </p>
+            </article>
+            <nav>
+                <ul>
+                    <li><a href="file:///Users/dalanlewis/Desktop/School%20Stuff/work%20experience.html">Work Experience</a></li>
+                    <li><a href="https://github.com/MarVlnMartian">Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/dalan-lewis-58a711174">linkedin.com</a></li>
+                </ul>
+            </nav>
+            <h5>
+                Contact Me!
+                </h5>
+            <form>
+                Name: <input type="text" /><br />
+                Email: <input type="text" /><br />
+                Request: <textarea rows="5" cols="40"></textarea><br />
+                <button>Submit!</button>
+            </form>
+            </div>  
+        )
+    }
+}
+
+export default aboutme
